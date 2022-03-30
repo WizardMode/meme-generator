fa1281862c2170a5ba4dbec4c1852987b41a34fe
WizardMode <humanspacebolt@live.com>
2022-03-30 13:27:16 -0700

    Meme component and form

---
+++ src/components/Meme.js
@@ -0,0 +1,27 @@
+import React from "react"
+
+function Meme() {
+    return (
+        <main>
+            <form className="form">
+                <input 
+                    type="text" 
+                    className="form-input"
+                    placeholder="Top text"
+                />
+                <input 
+                    type="text"
+                    className="form-input"
+                    placeholder="Bottom text"
+                 />
+                <button 
+                    className="form-button"
+                >
+                    Get new meme image 🖼
+                </button>
+            </form>
+        </main>
+    )
+}
+
+export default Meme
