d5d0bbba698f763623327731cd28dcad35153073
WizardMode <humanspacebolt@live.com>
2022-03-30 11:29:04 -0700

    Header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import './App.css';
+import React from "react"
+import Header from "./components/Header"
 
 function App() {
   return (
-    <div className="App">
-     <h1>Meme Generator</h1>
+    <div>
+     <Header />
     </div>
   );
 }
 
-export default App;
+export default App
