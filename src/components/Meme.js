9c5eebb1e06dd17a4577e88016bccbcf7ab38ffc
WizardMode <humanspacebolt@live.com>
2022-05-02 15:48:48 -0700

    get memes from API

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,27 +1,41 @@
 import React from "react"
-import memesData from "../memesData.js"
-
-// Challange:
-// 1. Set up the text inputs to save to
-//    the 'topText' and 'bottomText' state variables.
-// 2. Replace the hard-coded text on the image with 
-//   the text being saved to the state.
+// import memesData from "../memesData.js"
 
 function Meme() {
 
-    // const [memeImage, setMemeImage] = React.useState("")
+    // Challange: 
+    // As soon ass the Meme component loads the first time,
+    // make an API call to "https://api.imgflip.com/get_memes".
+    //
+    // When the data comes in, save just the memes array part
+    // of that data to the "allMemes" state
+    //
+    // Think about if theree are any dependencies that, if they
+    // change, you'd want to cause to re-run this function.
+    //
+    // Hint: for now, don't try to use an async/wait function.
+    // Instead, use '.then()' blocks to resolve the promises
+    // from using 'feth'. we'll learn why after this challange.
+
     const [meme, setMeme] = React.useState({
         topText: "",
         bottomText: "",
         randomImage: "http://i.imgflip.com/1bij.jpg"
     })
 
-    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
+    const [allMemes, setAllMemes] = React.useState([])
+
+    React.useEffect( () => {
+        fetch("https://api.imgflip.com/get_memes")
+            .then(res => res.json())
+            .then(data => setAllMemes(data.data.memes))
+    }, [])
+
+    // console.log(allMemes)
 
     function getMemeImage() {
-        const memesArray = allMemeImages.data.memes
-        const randomNumber = Math.floor(Math.random() * memesArray.length)
-        const url = memesArray[randomNumber].url
+        const randomNumber = Math.floor(Math.random() * allMemes.length)
+        const url = allMemes[randomNumber].url
         setMeme(prevMeme => ({
             ...prevMeme,
             randomImage: url
@@ -64,8 +78,6 @@ function Meme() {
                 </button>
                 <dvi className="meme">
                 <img className="meme-image" src={meme.randomImage} />
-                {/*<h2 className="meme-text top">One does not simply</h2>*/}
-                {/*<h2 className="meme-text bottom">Walk into Mordor</h2>*/}
                 <h2 className="meme-text top">{meme.topText}</h2>
                 <h2 className="meme-text bottom">{meme.bottomText}</h2>
                 </dvi>
